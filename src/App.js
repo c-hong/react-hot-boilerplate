3170f85d042e216e239d6ad59fdf62cd920bf772
c-hong <CSHong@31-35-187.wireless.csail.mit.edu>
2017-01-10 17:42:36 -0500

    everything is broken

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+//import Board from './Board';
+import Knight from './Knight';
 
 export default class App extends Component {
-  render() {
-    return (
-      <h1>Hello, world.</h1>
-    );
-  }
+	render() {
+		return (
+			<h1>hello world </h1>
+		);
+	}
 }
---
+++ src/Board.js
@@ -0,0 +1,44 @@
+import react, {Component, PropTypes} from 'react';
+import Square from './Square';
+import Knight from './Knight';
+
+export default class Board extends Component {
+	/*renderSquare(x, y) {
+		const black = (x + y) % 2 === 1;
+
+		const [knightX, knightY] = this.props.knightPosition;
+		const piece = (x === knightX && y === knightY) ?
+			<Knight /> :
+			null;
+
+		return (
+			<Square black={black}>
+				{piece}
+			</Square>
+		);
+	}
+
+	render() {
+		return (
+			<div style={{
+				width: '100%',
+				height: '100%'
+			}}>
+				{this.renderSquare(0, 0)}
+				{this.renderSquare(1, 0)}
+				{this.renderSquare(2, 0)}
+			</div>
+		);
+	}*/
+	render() {
+		return (
+			<h1>hello world </h1>
+		);
+	}
+}
+
+/*Board.propTypes = {
+	knightPosition: PropTypes.arrayOf(
+		PropTypes.number.isRequired
+	).isRequired
+};*/
